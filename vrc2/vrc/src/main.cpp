54273afd462fee248e87956e1b4f3afc6e3807e6
LobstersGH <Helloguy62@Outlook.com>
2026-07-31 09:15:57 -0700

    Update main.cpp

--- vrc2/vrc/src/main.cpp
+++ vrc2/vrc/src/main.cpp
@@ -42,20 +42,20 @@ lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
 // lateral motion controller
 lemlib::ControllerSettings linearController(10, // proportional gain (kP)
                                             0, // integral gain (kI)
-                                            3, // derivative gain (kD)
-                                            3, // anti windup
-                                            1, // small error range, in inches
-                                            100, // small error range timeout, in milliseconds
-                                            3, // large error range, in inches
-                                            500, // large error range timeout, in milliseconds
-                                            20 // maximum acceleration (slew)
+                                            0, // derivative gain (kD)
+                                            0, // anti windup
+                                            0, // small error range, in inches
+                                            0, // small error range timeout, in milliseconds
+                                            0, // large error range, in inches
+                                            0, // large error range timeout, in milliseconds
+                                            0 // maximum acceleration (slew)
 );
 
 // angular motion controller
-lemlib::ControllerSettings angularController(2, // proportional gain (kP)
+lemlib::ControllerSettings angularController(12.5,// proportional gain (kP)
                                              0, // integral gain (kI)
-                                             10, // derivative gain (kD)
-                                             3, // anti windup
+                                             20, // derivative gain (kD)
+                                             0, // anti windup
                                              1, // small error range, in degrees
                                              100, // small error range timeout, in milliseconds
                                              3, // large error range, in degrees
@@ -193,7 +193,7 @@ void autonomous() {
 	chassis.setPose(0, 0, 0);
     
     //Start your auto here! You can use the example autonomous routine, or write your own
-	
+	chassis.moveToPoint(0,24, 100000);
 
 }
 
